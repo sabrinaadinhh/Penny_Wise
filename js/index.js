64839d7e8f5c9e8332324b13d09e7bad59604d59
sabrinaadinhh <118635221+sabrinaadinhh@users.noreply.github.com>
2023-06-08 19:53:28 -0400

    more updates

---
+++ js/index.js
@@ -0,0 +1,10 @@
+console.log("Your index.js file is loaded correctly!");
+
+const toTop = document.querySelector(".to-top");
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 100) {
+    toTop.classList.add("active");
+  } else {
+    toTop.classList.remove("active");
+  }
+})
